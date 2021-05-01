7d5a47501321583867e42845444e6d3b78ce7e7e
saikr789 <81815804+saikr789@users.noreply.github.com>
2021-05-01 15:17:05 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -7,7 +7,6 @@ print(datetime.datetime.now())
 print(os.getcwd())
 print(os.listdir())
 print(multiprocessing.cpu_count())
-print(len(os.listdir("Data/Stock")))
 for dirname, _, filenames in os.walk(os.getcwd()):
     for filename in filenames:
         print(os.path.join(dirname, filename))
