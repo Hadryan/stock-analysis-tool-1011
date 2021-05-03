191f9b1b12b2b217fdb8bf6e46032fa665dc9aa1
saikr789 <saikrishna.nama@msitprogram.net>
2021-05-03 22:31:59 +0530

    simulation

--- app/pages/api/simulation.js
+++ app/pages/api/simulation.js
@@ -2,33 +2,55 @@ const fs = require("fs");
 const axios = require("axios");
 const csv = require("fast-csv");
 const moment = require("moment");
-let filepath =
-  "F:\\PracticumProject\\stock-analysis-tool-1011\\Data\\simulation\\secid_Next_30_days.csv";
 
 export default async (req, res, next) => {
   try {
+    let filepath =
+      "F:\\PracticumProject\\stock-analysis-tool-1011\\Data\\simulation\\secid_Next_30_days.csv";
     const companywithidURL =
       "https://raw.githubusercontent.com/saikr789/stock-analysis-tool-1011/master/Data/companywithid.json";
+    let simulationURL =
+      "https://raw.githubusercontent.com/saikr789/stock-analysis-tool-1011/master/Data/simulation/secid_Next_30_days.csv";
     console.log(req.url, req.query);
     const date = moment(req.query["date"], "DD-MM-YYYY").unix();
     const days = req.query["days"];
     const investment = parseFloat(req.query["investment"]);
     const company = req.query["company"].toUpperCase();
-    console.log(date, days, company, investment);
     axios
       .get(companywithidURL)
       .then(async (s) => {
-        const resp = s.data;
-        const secid = resp[company];
-        filepath = filepath.replace("secid", secid);
-        await compute(filepath, date, days, investment)
-          .then((s) => {
-            res.send(s);
-          })
-          .catch((e) => {
-            res.status(404).send({ error: "error" });
-            console.log(e);
-          });
+        if (s.status === 200) {
+          const resp = s.data;
+          const secid = resp[company];
+          await computeFromURL(
+            simulationURL.replace("secid", secid),
+            date,
+            days,
+            investment
+          )
+            .then((s) => {
+              res.send(s);
+            })
+            .catch((e) => {
+              res.status(404).send({ error: "error" });
+              console.log(e);
+            });
+          // await compute(
+          //   filepath.replace("secid", secid),
+          //   date,
+          //   days,
+          //   investment
+          // )
+          //   .then((s) => {
+          //     res.send(s);
+          //   })
+          //   .catch((e) => {
+          //     res.status(404).send({ error: "error" });
+          //     console.log(e);
+          //   });
+        } else {
+          res.status(404).send({ error: "error" });
+        }
       })
       .catch((e) => {
         res.status(404).send({ error: "error" });
@@ -113,6 +135,87 @@ const compute = async (filepath, date, days, investment) => {
           reject({});
         }
         console.log(e);
+      });
+  });
+};
+
+const computeFromURL = async (simulationURL, date, days, investment) => {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(simulationURL)
+      .then((s) => {
+        if (s.status == 200) {
+          let response = [];
+          let start = false;
+          let num = 0;
+          let invested = false;
+          let shares = 0;
+          let rows = s.data.split("\n");
+          const header = rows[0].split(",");
+          const investindex = header.indexOf("invest");
+          const exitindex = header.indexOf("exit");
+          const dateindex = header.indexOf("date");
+          const closeindex = header.indexOf("close");
+          for (let i = 1; i < rows.length; i++) {
+            const data = rows[i].split(",");
+            const cur = moment(data[dateindex], "YYYY-MM-DD").unix();
+            if (cur == date) {
+              start = true;
+            }
+
+            if (start) {
+              num = num + 1;
+              if (data[investindex] === "True") {
+                if (invested == false) {
+                  shares = investment / parseFloat(data[closeindex]);
+                  invested = true;
+                  response.push({
+                    investment: investment,
+                    entrydate: data[dateindex],
+                    shares: shares,
+                  });
+                }
+              }
+              if (data[exitindex] === "True") {
+                if (invested) {
+                  investment = parseFloat(data[closeindex]) * shares;
+                  response.push({
+                    investment: investment,
+                    exitdate: data[dateindex],
+                    shares: shares,
+                  });
+                  invested = false;
+                }
+              }
+              if (num == days) {
+                if (invested) {
+                  investment = parseFloat(data[closeindex]) * shares;
+                  invested = false;
+                  response.push({
+                    investment: investment,
+                    exitdate: data[dateindex],
+                    shares: shares,
+                  });
+                }
+                break;
+              }
+            }
+          }
+          if (invested) {
+            investment = parseFloat(data[closeindex]) * shares;
+            response.push({
+              investment: investment,
+              exitdate: data[dateindex],
+              shares: shares,
+            });
+            invested = false;
+          }
+          resolve(response);
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+        reject({});
       });
   });
 };
