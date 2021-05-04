e4d734761e63f723a1fb48731d162453e123fb08
saikr789 <81815804+saikr789@users.noreply.github.com>
2021-05-04 19:36:41 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -5,7 +5,6 @@ import subprocess
 import pandas as pd
 print(datetime.datetime.now())
 print(os.getcwd())
-print(os.listdir())
 print(multiprocessing.cpu_count())
 for dirname, _, filenames in os.walk(os.getcwd()):
     for filename in filenames:
