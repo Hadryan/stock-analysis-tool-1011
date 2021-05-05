d97ccbb0d1050270802dfaebfc3e60e0b94ce861
saikr789 <81815804+saikr789@users.noreply.github.com>
2021-05-05 13:19:46 +0530

    Update previousdaystockdetails.py

--- previousdaystockdetails.py
+++ previousdaystockdetails.py
@@ -2,11 +2,13 @@ import pandas as pd
 import os
 import subprocess
 
-subprocess.run(["git", "config","--global", "user.email", "saikrishna.nama@msitprogram.net"])
-subprocess.run(["git", "config","--global", "user.name", "saikr789"])
-subprocess.run(["git", "pull", "origin", "master"])
+# subprocess.run(["git", "config","--global", "user.email", "saikrishna.nama@msitprogram.net"])
+# subprocess.run(["git", "config","--global", "user.name", "saikr789"])
+# subprocess.run(["git", "pull", "origin", "master"])
 
-path = os.path.join(os.getcwd(), "Data")
+
+path = os.path.join(os.getcwd(), "Data","Stock")
+
 # previousdaystockdetails = pd.DataFrame()
 # for name in os.listdir(path):
 #     try:
@@ -16,10 +18,10 @@ path = os.path.join(os.getcwd(), "Data")
 #     except:
 #         pass
 previousdaystockdetails = pd.read_csv(os.path.join(path,"results.csv"))
-previousdaystockdetails.to_csv(os.path.join(
-    path,"Stock", "previousdaystockdetails.csv"), index=None)
-subprocess.run(["git", "add", os.path.join(
-    path, "previousdaystockdetails.csv")])
-subprocess.run(["git", "commit", "-m", "previousdaystockdetails"])
-subprocess.run(["git", "pull", "origin", "master"])
-subprocess.run(["git", "push", "origin", "master"])
+previousdaystockdetails.to_csv(os.path.join(path,"previousdaystockdetails.csv"), index=None)
+
+# subprocess.run(["git", "add", os.path.join(
+#     path, "previousdaystockdetails.csv")])
+# subprocess.run(["git", "commit", "-m", "previousdaystockdetails"])
+# subprocess.run(["git", "pull", "origin", "master"])
+# subprocess.run(["git", "push", "origin", "master"])
